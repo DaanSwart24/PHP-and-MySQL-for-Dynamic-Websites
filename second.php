483e6064642b3d3abe3695c514a06d5f489ebd18
Daan Swart <daanswart24@gmail.com>
2024-12-19 16:10:59 +0100

    Sending Data to the Web Browser

---
+++ second.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Using Echo</title>
+</head>
+<body>
+    <!-- Script 1.3 - second-php -->
+     <p>This is standard html</p>
+    <?php
+    echo 'This was generated using php!' 
+    ?>
+</body>
+</html>
