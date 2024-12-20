a903c3a1b65436b9ea9aa30c421b9424536e3f3e
Daan Swart <daanswart24@gmail.com>
2024-12-20 14:34:49 +0100

    Introducing Strings

---
+++ strings.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strings</title>
+</head>
+<body>
+<?php # Script 1.6 - strings.php
+
+// Create the variables
+$first_name = 'Bret Easton';
+$last_name = 'Ellis';
+$book = 'American Psycho';
+
+// Print the values
+echo "<p>The book <em>$book</em> was written by $first_name $last_name.";
+
+?>
+</body>
+</html>
